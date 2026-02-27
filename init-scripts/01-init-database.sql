5544b826c9191bcd7d74b23cc9d515e344e2cb5d
westb_t9hq80u <Westbyemmikael3@gmail.com>
2026-02-27 14:52:50 +0100

    satt inn testdata i init script

--- init-scripts/01-init-database.sql
+++ init-scripts/01-init-database.sql
@@ -4,11 +4,21 @@
 -- ============================================================================
 
 -- Opprett grunnleggende tabeller
+CREATE TABLE IF NOT EXISTS kunder (
+    kunde_id INT,
+    fornavn VARCHAR(50),
+    etternavn VARCHAR(50),
+    epost VARCHAR(100),
+    mobilnummer INT,
+    opprettet TIMESTAMP
+);
+
 
 
 
 -- Sett inn testdata
-
+INSERT INTO kunder (kunde_id, fornavn, etternavn) VALUES
+    (1, John, Doe);
 
 
 -- DBA setninger (rolle: kunde, bruker: kunde_1)
